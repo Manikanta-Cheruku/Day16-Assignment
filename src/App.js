25a28f49bab9333be65506481951935aa12d576e
manikantacheruku <manikantacheruku0308@gmail.com>
2025-03-16 17:48:47 +0530

    day 16 assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
+import EmployeeForm from './components/EmployeeForm';
+import Login from './components/Login';
+import Header from './components/Header';
 
+/**
+ * App Component
+ * Sets up routing for the application and includes the header for authenticated pages.
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-4">
+        {/* Header will render except on the login page */}
+        <Header />
+        <Routes>
+          {/* Route for login */}
+          <Route path="/login" element={<Login />} />
+          {/* Routes for employee management */}
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/add" element={<EmployeeForm />} />
+          <Route path="/edit/:id" element={<EmployeeForm />} />
+          {/* Redirect unknown routes */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
